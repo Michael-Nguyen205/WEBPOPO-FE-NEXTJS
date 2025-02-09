f490cfa82bdd7d4ce77f7d4bdcb4f1179f84c997
Michael-Nguyen205 <binduykhanhvn@gmail.com>
2025-02-09 15:23:45 +0700

    ề

--- src/app/[locale]/(publics)/components/welcome/Welcome.tsx
+++ src/app/[locale]/(publics)/components/welcome/Welcome.tsx
@@ -29,6 +29,21 @@ const Welcome = () => {
           Join us at Studio Agatho and be part of a fulfilling and enriching experience where you'll be valued, supported,
           and inspired to reach your full potential.
         </p>
+
+
+
+        {/* CHÀO MỪNG ĐẾN VỚI WEBPOPO STUDIO
+Tại WebPopo Studio, chúng tôi đam mê tạo ra những trải nghiệm số sáng tạo giúp doanh nghiệp phát triển. Chúng tôi chuyên thiết kế và phát triển website & ứng dụng, không chỉ đẹp mắt mà còn tối ưu hóa trải nghiệm người dùng một cách mượt mà nhất.
+
+Chúng tôi tin rằng những sản phẩm số tuyệt vời được tạo ra từ sự hợp tác, sáng tạo và công nghệ tiên tiến. Dù đó là một website chuyên nghiệp, một ứng dụng web mạnh mẽ hay một app di động đa tính năng, chúng tôi luôn biến ý tưởng thành hiện thực với độ chính xác và hiệu quả cao.
+
+🌟 Vì sao chọn WebPopo Studio?
+✅ Phát triển website & ứng dụng theo yêu cầu
+✅ Giải pháp tối ưu, thân thiện với người dùng
+✅ Công nghệ hiện đại, xu hướng mới nhất
+✅ Hỗ trợ & bảo trì tận tâm
+
+Hãy đồng hành cùng chúng tôi để biến ý tưởng của bạn thành giải pháp số đột phá. Cùng nhau tạo nên những sản phẩm tuyệt vời! 🚀 */}
       </div>
     </div>
   );
